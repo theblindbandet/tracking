2301333214122f6ff18329f8c88e52961b6ceacf
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-06 16:54:32 -0400

    add user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,12 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   # PATCH/PUT /users/1
