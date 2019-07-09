902b8c9cedfe1a254387487ff4533e8565a0a91f
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-09 17:46:30 -0400

    add sessions model

---
+++ test/models/session_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class SessionTest < ActiveSupport::TestCase
+  def setup
+    @user = users(:mordan)
+    @session = Session.new(user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @session.valid?
+  end
+
+  test "user id should be present" do
+    @session.user_id = nil
+    assert_not @session.valid?
+  end
+end
