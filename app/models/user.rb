91e1dfee25109b85977ba77dc952232b73aa98e6
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-09 17:47:27 -0400

    add session and digest method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one :session, dependent: :destroy
   before_save {email.downcase!}
 
   validates :name, presence: true, length: {minimum: 3, maximum: 50}
@@ -7,4 +8,10 @@ class User < ApplicationRecord
             format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   has_secure_password
   validates :password, presence: true, length: {minimum: 6}
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
