eaec649829be57049e3d6c909551f6d26072d159
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-10 19:32:34 -0400

    add belongs_to user relationship

--- db/migrate/20190710212822_create_login_records.rb
+++ db/migrate/20190710212822_create_login_records.rb
@@ -1,6 +1,7 @@
 class CreateLoginRecords < ActiveRecord::Migration[5.2]
   def change
     create_table :login_records do |t|
+      t.belongs_to :user, index: true
       t.date :date
       t.time :logged_in_at
       t.time :logged_out_at
