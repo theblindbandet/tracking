d163a316a289a3760070391609bd31a6faf0c46f
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-10 19:36:09 -0400

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_194551) do
+ActiveRecord::Schema.define(version: 2019_07_10_212822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "login_records", force: :cascade do |t|
+    t.bigint "user_id"
+    t.date "date"
+    t.time "logged_in_at"
+    t.time "logged_out_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_login_records_on_user_id"
+  end
 
   create_table "sessions", force: :cascade do |t|
     t.bigint "user_id"
