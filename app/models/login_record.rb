40ca3ed3ca086800f5f98062c3617d8f5c8c7368
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-11 12:31:04 -0400

    add more data validation and tests

--- app/models/login_record.rb
+++ app/models/login_record.rb
@@ -1,4 +1,7 @@
 class LoginRecord < ApplicationRecord
   belongs_to :user
-  validates :user_id, presence: true
+  validates :user_id,       presence: true
+  validates :date,          presence: true
+  validates :logged_in_at,  presence: true
+  validates :logged_out_at, presence: true
 end
--- test/models/login_record_test.rb
+++ test/models/login_record_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class LoginRecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:mordan)
+    @login_record = LoginRecord.new(user_id: @user.id, date: Date::today, 
+                                    logged_in_at: Time.now, logged_out_at: Time.now + 1)
+  end
+
+  test "should be valid" do
+    assert @login_record.valid?
+  end
+
+  test "date should be present" do
+    @login_record.date = nil
+    assert_not @login_record.valid?
+  end
+
+  test "logged_in_at should be present" do
+    @login_record.logged_in_at = nil
+    assert_not @login_record.valid?
+  end
+  
+  test "logged_out_at should be present" do
+    @login_record.logged_out_at = nil
+    assert_not @login_record.valid?
+  end
+
+  test "user id should be present" do
+    @login_record.user_id = nil
+    assert_not @login_record.valid?
+  end
 end
