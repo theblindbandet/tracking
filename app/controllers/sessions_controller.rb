d7a38cc956c859a88f472c5772c7bd010b1edd99
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-11 14:04:30 -0400

    change sessions_controller to use the new start_time method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,9 +13,8 @@ class SessionsController < ApplicationController
   def destroy
     user = User.find(params[:id])
     if logged_in? user
-      login_time = Time.parse(user.session.created_at.to_s)
       record = user.login_records.build(date: Date::today, 
-                                        logged_in_at: login_time, 
+                                        logged_in_at: user.session.start_time, 
                                         logged_out_at: Time.now)
       record.save
       log_out user 
