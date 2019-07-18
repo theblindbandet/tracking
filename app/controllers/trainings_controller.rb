d646abb2d6ec1aa5d9721f81ee473deaf0d5cdbd
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-18 16:43:21 -0400

    add training show page

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,10 +1,25 @@
 class TrainingsController < ApplicationController
   def show
+    @training = Training.find(params[:id])
   end
 
   def new
+    @training = Training.new
   end
 
   def create
+    @training = Training.new(training_params)
+    if @training.save
+      flash[:success] = "Successfully created!"
+      redirect_to @training
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def training_params
+      params.require(:training).permit(:title, :description, :length)
+    end
 end
