5b37fc81cf06ddf290a650124c94f13e2f8083cb
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-18 16:43:21 -0400

    add training show page

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,9 +1,27 @@
 # frozen_string_literal: true
 
 class TrainingsController < ApplicationController
-  def show; end
+  def show
+    @training = Training.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @training = Training.new
+  end
 
-  def create; end
+  def create
+    @training = Training.new(training_params)
+    if @training.save
+      flash[:success] = "Successfully created!"
+      redirect_to @training
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def training_params
+    params.require(:training).permit(:title, :description, :length)
+  end
 end
