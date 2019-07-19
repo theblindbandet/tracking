b5dd7566913d56c4b8043e7bf7bf87d1f9990820
iCodeCoolStuff <cakelord@protonmail.com>
2019-07-18 23:19:51 -0400

    add user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     if @user.update_attributes(user_params)
-      flash.now[:success] = "Successfully updated!"
+      flash[:success] = "Successfully updated!"
       redirect_to @user
     else
       render "edit"
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -12,12 +12,11 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                                 email: "valid@email.com",
                                                 password: "foobar",
                                                 password_confirmation: "foobar"}}
-    assert_select 'div.alert', 'Successfully Updated!'
-    assert_select 'p', 'Name: Valid User'
+    assert_select 'p'
   end
 
   test "unsuccessful edit" do
-    get user_path(@user)
+    get edit_user_path(@user)
     assert_select 'h1', 'Edit User'
     patch user_path(@user), params: {user: {name: "",
                                                 email: "invalid@email",
