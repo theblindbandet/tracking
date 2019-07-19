556e3362801f582e5cd266becd73caf26d67f2c6
Programmer <programmer@redalert1741.org>
2019-07-18 20:30:50 -0400

    add training edit page

---
+++ test/integration/trainings_edit_test_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class TrainingsEditTestTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @training = trainings(:meme_creation)
+  end
+
+  test "successful edit" do
+    get edit_training_path(@training)
+    assert_select 'h1', 'Edit Training'
+    patch training_path(@training), params: {training: {title: "Meme Destruction",
+                                                    description: "A course that tells you how to destroy good memes and replace them
+                                                                  with stale ones",
+                                                    length: 2.5}}
+    assert_select 'div.alert', 'Successfully Updated!'
+    assert_select 'input[value="Meme Destruction"]'
+  end
+
+  test "unsuccessful edit" do
+    get edit_training_path(@training)
+    assert_select 'h1', 'Edit Training'
+    patch training_path(@training), params: {training: {title: "",
+                                                    description: 123,
+                                                    length: "akjldfka"}}
+    assert_select 'h1', 'Edit Training'
+  end
+end
