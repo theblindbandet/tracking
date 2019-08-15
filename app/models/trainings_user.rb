6701cc814f28f55015b5855a5dd370a2ecedceaa
Jordan Miller <jmiller@springbuk.com>
2019-08-15 19:59:18 -0400

    Fixed failed merge commit

--- app/models/trainings_user.rb
+++ app/models/trainings_user.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 # frozen_string_literal: true
 
-=======
->>>>>>> add training model
 class TrainingsUser < ApplicationRecord
   belongs_to :training
   belongs_to :user
--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,10 @@
 
 Rails.application.routes.draw do
   resources :trainings
-<<<<<<< HEAD
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
   get    "/logout",  to: "sessions#logout"
   post   "/logout",  to: "sessions#destroy"
-=======
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  get    '/logout',  to: 'sessions#logout'
-  post   '/logout',  to: 'sessions#destroy'
->>>>>>> add training model
   resources :users
   resources :sessions, only: %i(new create destroy) do
     member do
