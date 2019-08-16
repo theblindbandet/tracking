f53de4493a4138b601028318ac4e762120fc9b7c
Jordan Miller <jmiller@springbuk.com>
2019-08-15 20:27:48 -0400

    More formatting things

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -1,4 +1,6 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class UsersEditTest < ActionDispatch::IntegrationTest
   def setup
@@ -7,21 +9,21 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "successful edit" do
     get edit_user_path(@user)
-    assert_select 'h1', 'Edit User'
-    patch user_path(@user), params: {user: {name: "Valid User",
-                                                email: "valid@email.com",
-                                                password: "foobar",
-                                                password_confirmation: "foobar"}}
-    assert_select 'p'
+    assert_select "h1", "Edit User"
+    patch user_path(@user), params: { user: { name: "Valid User",
+                                              email: "valid@email.com",
+                                              password: "foobar",
+                                              password_confirmation: "foobar" } }
+    assert_select "p"
   end
 
   test "unsuccessful edit" do
     get edit_user_path(@user)
-    assert_select 'h1', 'Edit User'
-    patch user_path(@user), params: {user: {name: "",
-                                                email: "invalid@email",
-                                                password: "akjldfka",
-                                                password_confirmation: ""}}
-    assert_select 'h1', 'Edit User'
+    assert_select "h1", "Edit User"
+    patch user_path(@user), params: { user: { name: "",
+                                              email: "invalid@email",
+                                              password: "akjldfka",
+                                              password_confirmation: "" } }
+    assert_select "h1", "Edit User"
   end
 end
